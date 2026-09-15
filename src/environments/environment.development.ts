d229f309ace171141a9df31e8837d2478de2d9d1
Mathias Mayer <mayer.mathias@proton.me>
2026-09-15 11:12:12 +0200

    add: environments for firebase

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDE__dZsnsYIOpwYpFcl36T-_sxFUP5eXg',
+    authDomain: 'dabub-417e9.firebaseapp.com',
+    projectId: 'dabub-417e9',
+    storageBucket: 'dabub-417e9.firebasestorage.app',
+    messagingSenderId: '628896026217',
+    appId: '1:628896026217:web:3bb6d6fcfafeaafe609cf8',
+    measurementId: 'G-XHPKCSK1HM',
+  },
+};
