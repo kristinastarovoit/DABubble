129935cde88c69395bc3b916e5cf2d02c8dfdd19
Anne Manthey <anne.manthey@pm.me>
2026-09-15 11:51:07 +0200

    feat(header): add base structure

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
 
 @Component({
-  imports: [],
+  imports: [CommonModule, FormsModule],
   selector: 'app-header',
   styleUrl: './header.scss',
   templateUrl: './header.html',
 })
-export class Header {}
+export class Header {
+  // currentUser!: User; // TODO: Ergänzen aus entsprechendem Service
+  searchQuery = '';
+  isUserMenuOpen = false;
+
+  toggleUserMenu(): void {
+    this.isUserMenuOpen = !this.isUserMenuOpen;
+  }
+
+  onSearchInput(): void {
+    // TODO: SearchService o.ä.
+  }
+
+  goToProfile(): void { /* Router-Navigation */ }
+  logout(): void { /* AuthService.logout() */ }
+}
+
