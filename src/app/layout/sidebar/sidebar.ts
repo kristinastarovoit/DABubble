3ef3b90bc80ea3932b5d9c341074e5137e61c0f4
Anne Manthey <anne.manthey@pm.me>
2026-09-15 12:47:44 +0200

    feat(sidebar): add basic elements

--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
-  imports: [],
+  imports: [CommonModule],
   selector: 'app-sidebar',
   styleUrl: './sidebar.scss',
   templateUrl: './sidebar.html',
 })
-export class Sidebar {}
+export class Sidebar {
+  // @Input() channels: Channel[] = [];
+  // @Input() directMessages: User[] = [];
+  @Input() activeChannelId: string | null = null;
+  @Input() activeDmId: string | null = null;
+
+  // @Output() channelSelected = new EventEmitter<Channel>();
+  // @Output() directMessageSelected = new EventEmitter<User>();
+  @Output() channelCreateRequested = new EventEmitter<void>();
+  @Output() workspaceEditRequested = new EventEmitter<void>();
+
+  isChannelsOpen = true;
+  isDirectMessagesOpen = true;
+
+  toggleChannels(): void {
+    this.isChannelsOpen = !this.isChannelsOpen;
+  }
+
+  toggleDirectMessages(): void {
+    this.isDirectMessagesOpen = !this.isDirectMessagesOpen;
+  }
+
+  // selectChannel(channel: Channel): void {
+  //   this.channelSelected.emit(channel);
+  // }
+
+  // selectDirectMessage(contact: User): void {
+  //   this.directMessageSelected.emit(contact);
+  // }
+
+  openChannelCreate(): void {
+    this.channelCreateRequested.emit();
+  }
+
+  openWorkspaceEdit(): void {
+    this.workspaceEditRequested.emit();
+  }
+}
