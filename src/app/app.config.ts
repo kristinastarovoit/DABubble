dd183b696f374e87c107a0be92c03ee161d81723
Mathias Mayer <mayer.mathias@proton.me>
2026-09-15 13:25:14 +0200

    add: auth service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,30 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  InjectionToken,
+  inject,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { FirebaseApp, initializeApp } from 'firebase/app';
+import { getAuth, Auth } from 'firebase/auth';
 
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
+
+export const FIREBASE_APP = new InjectionToken<FirebaseApp>('firebase.app');
+export const FIREBASE_AUTH = new InjectionToken<Auth>('firebase.auth');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    {
+      provide: FIREBASE_APP,
+      useFactory: () => initializeApp(environment.firebase),
+    },
+    {
+      provide: FIREBASE_AUTH,
+      useFactory: () => getAuth(inject(FIREBASE_APP)),
+    },
+  ],
 };
