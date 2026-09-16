10a7b56cfdc0fee2069e559b1f30fe20abe90585
kristina <kristina.starovoit@gmail.com>
2026-09-16 13:19:51 +0200

    feat: enhance ChannelService with user authentication and channel member management

--- src/app/shared/services/channel-service.ts
+++ src/app/shared/services/channel-service.ts
@@ -1,11 +1,12 @@
 import { Service, inject, signal } from '@angular/core';
-import { FIREBASE_FIRESTORE } from '../../app.config';
+import { FIREBASE_FIRESTORE, FIREBASE_AUTH } from '../../app.config';
 import { Channel } from '../interfaces/channel';
-import { collection, onSnapshot, addDoc, doc, updateDoc } from "firebase/firestore";
+import { collection, onSnapshot, addDoc, doc, updateDoc, deleteDoc, arrayUnion, arrayRemove } from "firebase/firestore";
 
 @Service()
 export class ChannelService {
     private db = inject(FIREBASE_FIRESTORE);
+    private auth = inject(FIREBASE_AUTH);
     channels = signal<Channel[]>([]);
 
     constructor() {
@@ -22,13 +23,27 @@ export class ChannelService {
         });
     }
 
-    // missing: memberIds, createdBy
     async addChannel(name: string, description: string) {
-        const docRef = await addDoc(collection(this.db, "channels"), {
+        const user = this.auth.currentUser?.uid;
+        if (!user) { return }
+
+        const channelRef = await addDoc(collection(this.db, "channels"), {
             name: name,
-            description: description
+            description: description,
+            createdBy: user,
+            memberIds: [user]
         });
-        console.log("Document written with ID: ", docRef.id);
+        console.log("Channel written with ID: ", channelRef.id);
+    }
+
+
+    // noch anpassen, je nachdem welcher channel gerade offen ist / ob einer offen ist
+    async addMemberToChannel(memberIds: string[], channelId: string) {
+        const channelRef = doc(this.db, "channels", channelId);
+        await updateDoc(channelRef, {
+            memberIds: arrayUnion(...memberIds)
+        });
+        console.log("User for channel written with ID: ", channelRef.id);
     }
 
     async editChannelName(name: string, channelId: string) {
@@ -45,9 +60,30 @@ export class ChannelService {
         });
     }
 
+    async leaveChannel(channelId: string) {
+        const user = this.auth.currentUser?.uid;
+        if (!user) { return };
+        const channelRef = doc(this.db, "channels", channelId);
+        await updateDoc(channelRef, {
+            memberIds: arrayRemove(user)
+        });
+        console.log("User in channel removed with ID: ", channelRef.id);
+    }
+
+    // To delete an entire collection or subcollection in Cloud Firestore, 
+    // retrieve (read) all the documents within the collection or subcollection and delete them. 
+    // This process incurs both read and delete costs. If you have larger collections, you may 
+    // want to delete the documents in smaller batches to avoid out-of-memory errors. Repeat the 
+    // process until you've deleted the entire collection or subcollection.
+
+    // Deleting a collection requires coordinating an unbounded number of individual delete requests. 
+    // If you need to delete entire collections, do so only from a trusted server environment. 
+    // While it is possible to delete a collection from a mobile/web client, doing so has negative 
+    // security and performance implications.
+
+    async deleteChannel(channelId: string) {
+        await deleteDoc(doc(this.db, "channels", channelId));
+    }
+
     // addMembers
-    // leaveChannel
-    // deleteChannel
-
-
 }
