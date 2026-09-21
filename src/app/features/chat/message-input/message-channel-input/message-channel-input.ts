4d2cc907981578768153be8def8d0cfdc136be89
kristina <kristina.starovoit@gmail.com>
2026-09-21 13:04:21 +0200

    refactor(chat): remove unused message channel input and list components

--- src/app/features/chat/message-input/message-channel-input/message-channel-input.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-message-channel-input',
-  styleUrl: './message-channel-input.scss',
-  templateUrl: './message-channel-input.html',
-})
-export class MessageChannelInput {}
